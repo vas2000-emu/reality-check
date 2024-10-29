86c20f8481ac9bb8c8d563299a215bd3d8e187b2
vas2000-emu <vanbukum@emich.edu>
2024-10-28 21:18:36 -0400

    First app commit

---
+++ reality-check/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello Expo Web!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+    color: '#333',
+  },
+});
